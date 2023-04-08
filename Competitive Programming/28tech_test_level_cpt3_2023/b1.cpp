5000cf8ff347b251ef75b57800959cea07668604
An <antp9254@gmail.com>
2023-04-09 00:12:51 +0700

    cpp

--- Competitive Programming/28tech_test_level_cpt3_2023/b1.cpp
+++ Competitive Programming/28tech_test_level_cpt3_2023/b1.cpp
@@ -1,9 +1,52 @@
 // link: https://codeforces.com/group/cGzZeXNyzp/contest/433846/problem/B1
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int n, k;
+int a[1000010];
+
+long long totalGap(int left, int right)
+{
+    long long total_gap = 0;
+    for (int i = left; i <= right; i++)
+        total_gap += (a[right] - a[i]);
+    return total_gap;
+}
+
+int bsearch(int left, int right)
+{
+    int l = left, r = right, m, res = left;
+    while (l <= r)
+    {
+        m = (l + r) / 2;
+        long long total_gap = totalGap(left, m);
+        if (total_gap == k)
+            return m;
+        else if (total_gap < k)
+        {
+            res = m;
+            l = m + 1;
+        }
+        else
+            r = m - 1;
+    }
+    return res;
+}
 
 int main()
 {
-    
-    return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    sort(a + 1, a + 1 + n);
+
+    int index = bsearch(n / 2 + 1, n);
+    long long total_gap = totalGap(n / 2 + 1, index);
+    int res = a[index] + ((k - total_gap) / (index - n / 2));
+
+    cout << res << endl;
 }
--- Competitive Programming/28tech_test_level_cpt3_2023/b4.cpp
+++ Competitive Programming/28tech_test_level_cpt3_2023/b4.cpp
@@ -2,9 +2,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n, t, l, r;
+int a[1000000];
+long long res[1000001];
+
 int main()
 {
-    int n;
-    int a[]
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++)
+    {
+        scanf("%d", &a[i]);
+        res[i] = res[i-1] + a[i];
+    }
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d%d", &l, &r);
+        printf("%lld\n", res[r] - res[l-1]);
+    }
+
     return 0;
 }

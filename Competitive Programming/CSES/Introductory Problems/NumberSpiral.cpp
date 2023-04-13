898c498fab933a619a64d6741ac35c46d75215da
An <antp9254@gmail.com>
2023-04-13 19:56:34 +0700

    cpp

---
+++ Competitive Programming/CSES/Introductory Problems/NumberSpiral.cpp
@@ -0,0 +1,20 @@
+// link: https://cses.fi/problemset/task/1071
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t; 
+    cin >> t;
+    while (t--) {
+        ll x,y;
+        cin >> y >> x;
+        
+    }
+     
+    return 0;
+}
